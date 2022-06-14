5877260bce8bfb0647e5bd6292465430bc671058
rmatovu987 <rmatovu987@gmail.com>
2022-06-14 05:32:01 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import {createClient} from 'redis';
+import {promisify} from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => console.log(`Redis client not connected to the server: ${error}`))
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.expireAsync = promisify(this.client.expire).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return await this.getAsync(key);
+  }
+
+  async set(key, value, duration){
+    this.setAsync(key, value);
+    this.expireAsync(key, duration);
+  }
+
+  async del(key){
+    this.delAsync(key);
+  }
+}
+
+const client = new RedisClient()
+module.exports = client
