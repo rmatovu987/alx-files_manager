88ac31094d1be5224a1d393980f1a64988aee4ae
rmatovu987 <rmatovu987@gmail.com>
2022-06-15 23:00:55 +0300

    Redis utils

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,35 @@
-import express from 'express';
+import {
+  Router,
+} from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
-router.get('/status', ((request, response) => AppController.getStatus(request, response)));
-router.get('/stats', ((request, response) => AppController.getStats(request, response)));
-router.post('/users', ((request, response) => UsersController.postNew(request, response)));
-router.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
-router.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
-router.get('/users/me', ((request, response) => UsersController.getMe(request, response)));
+// check status and stats of db
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+// connect and disconnect user
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+// upload files
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+// publish and unpublish
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+// user
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+// file content
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-import router from './routes/index';
-
-const express = require('express');
+import express from 'express';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', router);
+app.use(express.urlencoded());
+app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-export default app;
+app.listen(port, () => console.log(`Server running on port ${port}`));
